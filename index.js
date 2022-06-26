299af5f0184497940e0399f537431673dc0ea555
Phakhawat Chullamonthon <phakhawat.chu@outlook.com>
2022-06-26 11:05:05 +0700

    feat: add file upload analysis

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var bodyParser = require("body-parser");
+const multer = require("multer");
+var cors = require("cors");
+require("dotenv").config();
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "uploads");
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + "-" + Date.now());
+    },
 });
 
+var upload = multer({ storage: storage });
 
+app.get("/", function (req, res) {
+    res.sendFile(process.cwd() + "/views/index.html");
+});
 
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+    const file = req.file;
+    res.send({
+        name: file.originalname,
+        type: file.mimetype,
+        size: file.size,
+    });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+    console.log("Your app is listening on port " + port);
 });
